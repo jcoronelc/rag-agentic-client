5e057668849bb534e9c3525aaa0630a000e5119d
jcoronel <juanchocoronel80@gamil.com>
2025-07-02 07:42:26 -0500

    client rag

--- client/app_rag.py
+++ client/app_rag.py
@@ -115,7 +115,7 @@ def get_response(chat_id, question):
 
     collection_name_active = "bdv2"
     collection_name_active_summary = "bdvs3"
-    use_memory = False
+    use_memory = True
 
     payload = {
         "model_llm_responses": selected_llm_model,
